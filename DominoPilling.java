c4c87ac8402b78e3d0d9817089e331aefebc53d7
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-02-26 23:35:14 +0530

    Add files via upload

---
+++ DominoPilling.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class DominoPilling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int d = (m*n)/2;
+        System.out.println(d);
+
+    }
+    
+}
