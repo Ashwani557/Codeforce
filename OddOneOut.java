1b02499c9b3f8371450d02c0e333b7e06af24e75
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-03-09 13:02:18 +0530

    Add files via upload

---
+++ OddOneOut.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddOneOut {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = a^b^c;
+            System.out.println(d);
+        }
+        
+    }
+    
+}
