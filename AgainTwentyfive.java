943f2d6cf265c00341ffab71f7d62af53fa393de
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-03-25 10:59:32 +0530

    Add files via upload

---
+++ AgainTwentyfive.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AgainTwentyfive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // int result = 1;
+        // for (int i = 0; i < n; i++) {
+        //     result *= 5;
+            
+        // }
+
+
+        System.out.println(25);
+    }
+    
+}
