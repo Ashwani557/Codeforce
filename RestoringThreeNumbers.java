a2d61dfffd32608959b19a2b01e3441bdba2ffa3
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-04-05 23:00:25 +0530

    Add files via upload

---
+++ RestoringThreeNumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class RestoringThreeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[4];
+
+        for(int i = 0; i < 4; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int sum = arr[3];
+
+        System.out.println((sum - arr[0]) + " " + (sum - arr[1]) + " " + (sum - arr[2]));
+    }
+}
