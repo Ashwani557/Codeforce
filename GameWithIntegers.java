5484f2e26a84b18a62b81b4c09659d5fdb601675
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-04-09 21:51:12 +0530

    Add files via upload

---
+++ GameWithIntegers.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class GameWithIntegers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for(int i = 0; i<t;i++){
+            int n = sc.nextInt();
+            if (n %3 != 0 ){
+                System.out.println("First");
+
+            }
+            else{
+                System.out.println("Second");
+            }
+
+        }
+    }
+    
+}
