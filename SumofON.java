f510faf5bb3eae4affb8a5ea135162698eae4a26
Ashwani Pandey <ayushkurana2283@gmail.com>
2026-05-30 13:21:15 +0530

    Add files via upload

---
+++ SumofON.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumofON {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            long n = sc.nextLong();
+            long k = sc.nextLong();
+
+            if (n >= k * k && n % 2 == k % 2) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
